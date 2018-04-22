ef1cb2d2910c1778889a64e4c52106832135abe2
George <grooney4@gmail.com>
2018-04-22 19:28:23 -0400

    Added navigation controller, and second view controller.  App now displays.

--- Emojii Dictionary/EmojiiTableViewController.swift
+++ Emojii Dictionary/EmojiiTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class EmojiiTableViewController: UITableViewController {
-
+    var emojiis = [ " 😀", "❤️", "🐶","🌈","🍎" ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,16 +17,19 @@ class EmojiiTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 20
+        return emojiis.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
 
-       cell.textLabel?.text = "Hello World"
+       cell.textLabel?.text = emojiis[indexPath.row]
         
 
         return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ourSegue", sender: nil)
     }
 }
