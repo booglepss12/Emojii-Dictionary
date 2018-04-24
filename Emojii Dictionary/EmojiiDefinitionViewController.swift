40a466e980a170ce4e180fdc32025c546c889c69
George <grooney4@gmail.com>
2018-04-23 20:50:09 -0400

    Added birthdate and category labels

--- Emojii Dictionary/EmojiiDefinitionViewController.swift
+++ Emojii Dictionary/EmojiiDefinitionViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class EmojiiDefinitionViewController: UIViewController {
+    @IBOutlet weak var birthLablel: UILabel!
     @IBOutlet weak var emojiiLabel: UILabel!
     @IBOutlet weak var emojiiDefinition: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     var emojii = " "
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +20,25 @@ class EmojiiDefinitionViewController: UIViewController {
         //[ " 😀", "❤️", "🐶","🌈","🍎" ]
         if emojiiLabel.text == " 😀"{
             emojiiDefinition.text = "A smiley face"
+          birthLablel.text = "2013"
+          categoryLabel.text = "Faces"
         }else if emojiiLabel.text == "❤️"{
             emojiiDefinition.text = "Heart"
+            birthLablel.text = "2017"
+            categoryLabel.text = "Expressions"
         }else if emojiiLabel.text == "🐶"{
             emojiiDefinition.text = "Puppy Dog"
+            birthLablel.text = "2013"
+            categoryLabel.text = "Animals"
         }else if  emojiiLabel.text == "🌈"{
             emojiiDefinition.text = "Rainbow"
+            birthLablel.text = "2018"
+            categoryLabel.text = "Nature"
         
         }else if emojiiLabel.text == "🍎"{
             emojiiDefinition.text = "Apple"
+            birthLablel.text = "2015"
+            categoryLabel.text = "Food"
         }
     }
 
